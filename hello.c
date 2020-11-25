cf95cfd0bb9ec3cf1458d2dbe398bba1507a2f5d
Mathieu Champion <mathieu.champion@hotmail.com>
2020-11-25 16:08:20 +0100

    changing hello.c to print the connected sensor

--- hello.c
+++ hello.c
@@ -2,12 +2,34 @@
 
 #include "ev3.h"
 #include "ev3_light.h"
+#include "ev3_port.h"
+#include "ev3_sensor.h"
 
 int main( void )
 {
+	char s[ 256 ];
+    	int val , n, i, ii;
 	printf( "Hello, OS!\n" );
 
 	ev3_init();
+	ev3_sensor_init();
+	printf( "Found sensors:\n" );
+    	for ( i = 0; i < DESC_LIMIT; i++ ) {
+       		if ( ev3_sensor[ i ].type_inx != SENSOR_TYPE__NONE_ ) {
+            		printf( "  type = %s\n", ev3_sensor_type( ev3_sensor[ i ].type_inx ));
+            		printf( "  port = %s\n", ev3_sensor_port_name( i, s ));
+            		if ( get_sensor_mode( i, s, sizeof( s ))) {
+                		printf( "  mode = %s\n", s );
+            		}		
+            		if ( get_sensor_num_values( i, &n )) {
+                		for ( ii = 0; ii < n; ii++ ) {
+                    			if ( get_sensor_value( ii, i, &val )) {
+                        			printf( "  value%d = %d\n", ii, val );
+                    			}	
+                		}
+            		}
+        	}
+    	}
 	switch ( get_light( LIT_LEFT )) {
 
 	case LIT_GREEN:
