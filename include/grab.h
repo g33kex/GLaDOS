190efcb4ea6cbbba9bea24c6d0aa6a476321e058
florianodacodor <florian.lmouel@gmail.com>
2020-12-09 18:20:52 +0000

    premier test dela pince

--- include/grab.h
+++ include/grab.h
@@ -1,4 +1,14 @@
-void lower(int value);
-void lift(int value);
-void open();
-void close();
+#ifndef GRAB_H
+#define GRAB_H
+
+#include <stdint.h>
+#include <unistd.h>
+
+void lower();
+void lift();
+void open_hand();
+void close_hand();
+
+uint8_t hand;
+uint8_t lever;
+#endif // GRAB_H
---
+++ src/grab.c
@@ -0,0 +1,39 @@
+#include "grab.h"
+
+#include "ev3.h"
+#include "ev3_tacho.h"
+
+#include <stdio.h>
+
+static void init_motor(uint8_t);
+static void run_motor(uint8_t, int, int);
+
+
+int main(void) {
+	if(ev3_tacho_init()==-1)
+		return -1;
+	if ( ev3_search_tacho_plugged_in(67,0, &hand, 0 )) {
+		init_motor(hand);
+		run_motor(hand,360, 500);
+	} else {			
+		printf( "LEGO_EV3_M_MOTOR 1 is NOT found\n" );		    
+	}
+	return 0;
+}
+
+static void run_motor(uint8_t motor, int rot, int speed) {
+	set_tacho_speed_sp(motor, speed);
+	set_tacho_position_sp(motor, rot);
+	set_tacho_command_inx(motor, TACHO_RUN_TO_REL_POS);
+}
+
+static void init_motor(uint8_t motor) {
+	int count_per_rot;
+	get_tacho_count_per_rot(motor, &count_per_rot);
+	printf("Tacho count for motor %d = %d\n",motor,count_per_rot);
+	//One tacho count is actuall one degree
+	//set_tacho_speed_sp(motor, max_speed/10);
+	set_tacho_ramp_up_sp(motor, 0);
+	set_tacho_ramp_down_sp(motor, 0);
+	set_tacho_stop_action_inx(motor, TACHO_BRAKE);
+}
