94172ccf97916dd5deaa5db7d2b796eb963e2f4e
JeanMichelCourge <mathieu.champion@hotmail.com>
2020-12-12 11:17:50 +0100

    test6 theorically ready

--- src/test_sensors.c
+++ src/test_sensors.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "ev3.h"
 #include "ev3_light.h"
@@ -36,18 +37,24 @@ int move_robot_to_random_cube(){
   Vector researchPoint = (Vector) {60,40};
   void move_to(researchPoint);
   bool cubeFound = false;
-  initialOrientation = robot_pos.rotation ;
-  while (!cubeFound &&  ) {
-    rotate_to
-    update_rotation();
+  initialOrientation = robot_pos.rotation ; //should be 0 for the test6..
+  int i;
+  Vector target;
+  for (i = 0; i < 35 && !cubeFound ; ++i  ) { //on lui fait faire 35 petites rotations sauf s'il trouve le cube avant
+    target = vector_add(vector_from_polar(50, i *10),researchPoint)
+    rotate_to(target);
     if (!strcmp("BLUE", get_color())){
       cubeFound = true;
     }
   }
   if (cubeFound) {
+    int angle = i * 10;
     printf("CUBE FOUND ! Going to cube\n", );
+    int magnitude = 20;
     while(get_distance() > 50){ //50mm, peut être moi, peut être plus
-      move_to()
+      target = vector_from_polar(magnitude,angle)
+      move_to(target)
+      magnitude = magnitude + 10;
     }
     printf("INCH WE ARE CLOSE TO THE RANDOM CUBE NOW\n", );
     return 1;
