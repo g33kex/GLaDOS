5e153bf6b19f167b942637b460bd098fb2780a47
Virgile Retault <virgile.retault@eurecom.fr>
2020-12-14 10:51:01 +0100

    fixed wrong angle in rotate

--- src/motion.c
+++ src/motion.c
@@ -172,7 +172,7 @@ void rotate(double angle) {
         Sleep ( 200 );
         int old_rotation = robot_pos.rotation;
         update_rotation();
-        angle=(angle-(old_rotation-robot_pos.rotation));
+        angle=(angle-(robot_pos.rotation-old));
 
         if(angle>0) {
             set_motors_duty(-INITIAL_DUTY, INITIAL_DUTY);
