031ee92236c2d4d8ba190f3a2d15b60d4ddc469c
JeanMichelCourge <mathieu.champion@hotmail.com>
2020-12-14 11:16:49 +0100

    test

--- src/test_sensors.c
+++ src/test_sensors.c
@@ -87,7 +87,7 @@ int drop_ball_in_pyramid(){
   printf("distance du cube : %d\n",get_distance() );
   if(ecart_de_distance > 0){
     printf("il faut avancer le robot de %d mm\n",ecart_de_distance );
-    aller_tout_drovoid aller_tout_droit(int time); //idemit(ecart_de_distance * 10 + 200);
+    aller_tout_droit(ecart_de_distance * 10 + 200); //idemit(ecart_de_distance * 10 + 200);
     printf("distance du cube : %d\n",get_distance() );
   } else {
     printf("il faut reculer le robot de %d mm\n",ecart_de_distance );
