dab1de814827a8d8746a1712b066c6029a1baa81
Florian <florian.lemouel@telecom-paris.fr>
2021-01-13 15:42:21 +0100

    ajout de commentaires dans grab.h

--- include/grab.h
+++ include/grab.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+
+/*variables*/
 #define LEVER_PORT 66
 #define HAND_PORT 67
 
@@ -16,16 +18,40 @@
 #define LEVER_MID -210
 #define LEVER_DOWN 0
 
+
+/*functions*/
+
+/**brings the hand to the lowest position**/
 void lower();
+
+/**brings the hand at intermediate position (in the cube but above the balls**/
 void lower_half();
+
+/**brings the hand at the highest position**/
 void lift();
+
+/**opens the hand**/
 void open_hand();
+
+/**closes the hand**/
 void close_hand();
+
+/**procedure that combines the previous functions to grab a ball when placed above the cube**/
 void grab();
 
+/**tries grabing a ball with the previous procedure until it has one in hand**/
+void grab_with_retry();
+
+/*init functions*/
+
+/**associates the motors to their ports**/
 bool grab_init();
+
+/**sets positions of the hand as lowest and closed (to execute only once at startup)**/
 bool grab_position_init();
 
+/*constants*/
 uint8_t hand;
 uint8_t lever;
+
 #endif // GRAB_H
